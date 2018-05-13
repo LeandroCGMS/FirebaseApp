5d01adc7f96b7b93ac681c598ebdcb9455dc1852
LeandroCGMS <leandrocgms1986@outlook.com>
2018-05-13 12:33:23 -0400

    Criando BD com objetos instanciados no Firebase.

--- app/src/main/java/firebaseapp/leandro/com/br/firebaseapp/MainActivity.java
+++ app/src/main/java/firebaseapp/leandro/com/br/firebaseapp/MainActivity.java
@@ -8,13 +8,28 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
-    private DatabaseReference firebaseReferencia = FirebaseDatabase.getInstance().getReference();
+    private DatabaseReference databaseReferencia = FirebaseDatabase.getInstance().getReference();
+    private DatabaseReference usuarioReferencia = databaseReferencia.child("usuarios");
+    private DatabaseReference produtoReferencia = databaseReferencia.child("produtos");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        firebaseReferencia.child("pontos").setValue("100");
+        Usuario usuario = new Usuario();
+        usuario.setNome("Ana Helena");
+        usuario.setSobrenome("Silva");
+        usuario.setSexo("Feminino");
+        usuario.setIdade(25);
+
+        Produto produto = new Produto();
+        produto.setDescricao("Galaxy Note Ultra Mega Power");
+        produto.setCor("Preto");
+        produto.setFabricante("Samsung");
+
+        usuarioReferencia.child("003").setValue( usuario );
+        produtoReferencia.child("001").setValue( produto );
+
     }
 }
